a04cb3084b5c614ae1205ca30d9fe8879b917c40
Priyanka Sonia <priyankasoniad99@gmail.com>
2020-03-23 19:45:28 +0800

    update edit profile

---
+++ app/src/main/java/com/example/myevent/metodePembayaran.java
@@ -0,0 +1,14 @@
+package com.example.myevent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class metodePembayaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metode_pembayaran);
+    }
+}
