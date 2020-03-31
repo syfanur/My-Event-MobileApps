5946bb881cb210658c18481ed0bbf7e034ec28a3
Priyanka Sonia <priyankasoniad99@gmail.com>
2020-03-31 11:39:36 +0800

    menambahkan fungsi onclick pada login page dan verifikasi pembayaran

--- app/src/main/java/com/example/myevent/Register.java
+++ app/src/main/java/com/example/myevent/Register.java
@@ -3,6 +3,8 @@ package com.example.myevent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
     }
 }
--- app/src/main/java/com/example/myevent/VerifikasiPembayaran.java
+++ app/src/main/java/com/example/myevent/VerifikasiPembayaran.java
@@ -2,7 +2,10 @@ package com.example.myevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VerifikasiPembayaran extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class VerifikasiPembayaran extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verifikasi_pembayaran);
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new DialogInterface.OnClickListener() {
+            public void onClick(View v) {
+
+
     }
 }
