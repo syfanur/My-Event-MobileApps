f2823aba277e45d7890a1acd50e8fccd033d9621
Syfanur <j0h@example.com>
2020-03-31 10:02:59 +0700

    Menghubungankan detail event ke transaksi pembayaran ref #28

--- app/src/main/java/com/example/myevent/desc_event.java
+++ app/src/main/java/com/example/myevent/desc_event.java
@@ -17,4 +17,10 @@ public class desc_event extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desc_event);
-    }}
+    }
+
+    public void attend(View view) {
+        Intent a = new Intent(getApplicationContext(),transaksi1.class);
+        startActivity(a);
+    }
+}
