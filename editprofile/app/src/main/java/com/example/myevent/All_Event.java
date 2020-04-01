523cfe5c5de851646dd900da2fdccba92a69bafb
unknown <chrisheryanda@gmail.com>
2020-04-01 20:47:55 +0700

    Hasil Searching ref #12

---
+++ editprofile/app/src/main/java/com/example/myevent/All_Event.java
@@ -0,0 +1,14 @@
+package com.example.myevent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class All_Event extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all__event);
+    }
+}
