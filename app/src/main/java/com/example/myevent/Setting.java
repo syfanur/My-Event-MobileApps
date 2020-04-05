ee2ecd1a9c8877b6027ac98a2824d5ebb2607ec9
Syfanur <j0h@example.com>
2020-04-05 15:28:23 +0700

    setting tema dark/night mode ref #29

--- app/src/main/java/com/example/myevent/Setting.java
+++ app/src/main/java/com/example/myevent/Setting.java
@@ -1,14 +1,38 @@
 package com.example.myevent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
 
 public class Setting extends AppCompatActivity {
+
+    public static boolean NightMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        SwitchCompat switchCompat = findViewById(R.id.switch_dark_mode);
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                NightMode = isChecked;
+                int delaytime = 200;
+                buttonView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(NightMode){
+                            getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        } else {
+                            getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        }
+                    }
+                }, delaytime);
+            }
+        });
     }
 }
