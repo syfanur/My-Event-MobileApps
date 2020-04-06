e326c915c37ec61c588cd9bc8eb1645525efac0c
Syfanur <j0h@example.com>
2020-04-06 10:04:40 +0700

    membuat navigasi alamat menggunakan google maps ref #19

--- app/src/main/java/com/example/myevent/desc_event.java
+++ app/src/main/java/com/example/myevent/desc_event.java
@@ -11,16 +11,50 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 public class desc_event extends AppCompatActivity {
+    /*Deklarasi variable*/
+    Button btn_navigasi;
+    String goolgeMap = "com.google.android.apps.maps"; // identitas package aplikasi google masps android
+    Uri gmmIntentUri;
+    Intent mapIntent;
+    String bandung = "-6.973700, 107.629233"; // koordinat
 
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desc_event);
+
+        // menyamakan variable pada layout activity_main.xml
+        btn_navigasi    = (Button) findViewById(R.id.btn_navigasi);
+
+        // tombol untuk menjalankan navigasi goolge maps intents
+        btn_navigasi.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Buat Uri dari intent string. Gunakan hasilnya untuk membuat Intent.
+                gmmIntentUri = Uri.parse("google.navigation:q=" + bandung);
+
+                // Buat Uri dari intent gmmIntentUri. Set action => ACTION_VIEW
+                mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+                // Set package Google Maps untuk tujuan aplikasi yang di Intent yaitu google maps
+                mapIntent.setPackage(goolgeMap);
+
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                } else {
+                    Toast.makeText(desc_event.this, "Google Maps Belum Terinstal. Install Terlebih dahulu.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
 
     }
 
